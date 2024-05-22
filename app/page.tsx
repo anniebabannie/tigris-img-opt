0b3ebe87572c7fa489f6a56d3fb164748c9ad8d3
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-05-22 12:27:28 -0700

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>I have an image called <code>clouds.jpg</code> in my Tigris bucket. Let's retrieve it as a smaller image.</p>
+      <p>I have an image named <code>clouds.jpg</code> in my Tigris bucket. Let's retrieve it as a smaller image.</p>
       <Image src="clouds.jpg" alt="logo" width={100} height={100} />
     </div>
   );
