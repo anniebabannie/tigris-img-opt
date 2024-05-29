f1607f1ad0781c6180a57598ef45947a8c7de551
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-05-29 13:22:12 -0700

    WIP

--- utils/customLoader.js
+++ utils/customLoader.js
@@ -1,3 +1,6 @@
 export default function customLoader({ src, width, quality }) {
-  return `https://cachefly-tigris.fly.dev/${src}${width ? `?width=${width}` : ''}${quality ? `&quality=${quality}` : ''}`;
+  const s = src;
+  const w = width ? `?width=${width}` : '';
+  const q = quality ? `&quality=${quality}` : '';
+  return `https://cachefly-tigris.fly.dev/${s}${w}${q}`;
 }
