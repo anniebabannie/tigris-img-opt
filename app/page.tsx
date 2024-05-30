8e624c2b2cc4d3d340c572e560c5f538e8dc4b31
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-05-30 12:23:43 -0700

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>I have an image named <code>clouds.jpg</code> in my Tigris bucket. Let's retrieve it as a smaller image.</p>
+    <div className="flex justify-center items-center flex-col min-h-screen gap-6">
+      <p className="text-xl">I have an image named <code>clouds.jpg</code> in my Tigris bucket.</p>
+      <p className="text-xl">Let's retrieve it as a smaller image.</p>
       <Image src="clouds.jpg" alt="logo" width={100} height={100} />
     </div>
   );
